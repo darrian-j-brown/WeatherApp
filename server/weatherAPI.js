c6761a059fe89374b51498a988b5ee738a70493a
Darrian Brown <darrian.jordan.brown@gmail.com>
2020-03-18 20:58:34 -0500

    added handleOnSubmit to capture input added handleGeosuggestChange for input change added checkInput function to check if input is zip, city, or coordinates
    updated updateWeatherByCoords, onSuggestSelect, and getCurrentPos to handle data object instead of lat and lng

--- server/weatherAPI.js
+++ server/weatherAPI.js
@@ -1,20 +1,52 @@
 let axios = require("axios");
 
 //Build our OpenWeatherAPI Url
-let getUrl = (type, lat, lon) => {
+// let getUrl = (type, lat, lon) => {
+//   let url = "";
+//   url += "https://api.openweathermap.org/data/2.5/" + type;
+//   url += "?lat=" + lat;
+//   url += "&lon=" + lon;
+//   url += "&appid=b48bb8b80a8a300bdae80be70fa028d9";
+
+//   return url;
+// };
+
+let getUrl = (type, data) => {
+  console.log('data', data);
+  const { lat, lon, city, zip} = data;
   let url = "";
   url += "https://api.openweathermap.org/data/2.5/" + type;
-  url += "?lat=" + lat;
-  url += "&lon=" + lon;
+  if(lat && lon) {
+    url += "?lat=" + lat;
+    url += "&lon=" + lon;
+  } else if(city) {
+    url += "?q=" + city;
+  } else if(zip) {
+    url += "?zip=" + zip;
+  }
   url += "&appid=b48bb8b80a8a300bdae80be70fa028d9";
-
   return url;
 };
 
 //Async API Call
-let getData = (type, lat, lon) =>
+// let getData = (type, lat, lon) =>
+//   new Promise((resolve, reject) => {
+//     let url = getUrl(type, lat, lon);
+
+//     axios
+//       .get(url)
+//       .then(res => {
+//         resolve(res.data);
+//       })
+//       .catch(err => {
+//         reject(err.message);
+//       });
+//   });
+
+  let getData = (type, data) =>
   new Promise((resolve, reject) => {
-    let url = getUrl(type, lat, lon);
+    let url = getUrl(type, data);
+    console.log('url', url);
 
     axios
       .get(url)
@@ -22,18 +54,20 @@ let getData = (type, lat, lon) =>
         resolve(res.data);
       })
       .catch(err => {
-        reject(err.message);
+        reject('server error ', err.message);
       });
   });
 
 //Get Weather and Forecast Parallel, and send to client
 module.exports = async (req, res) => {
   //Get coordinates from request
-  let { lat, lon } = req.query;
+  let { lat, lon, city, zip } = req.query;
+  let data = req.query;
+
 
   //Call APIs
-  let weather = getData("weather", lat, lon);
-  let forecast = getData("forecast", lat, lon);
+  let weather = getData("weather", data);
+  let forecast = getData("forecast", data);
 
   //Parallel API Await
   [weather, forecast] = [await weather, await forecast];
@@ -44,5 +78,5 @@ module.exports = async (req, res) => {
     forecast
   });
 
-  console.log("Got Weather Data for", weather.name);
+  console.log("Got Weather Data for", forecast);
 };
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Geosuggest from "react-geosuggest";
 
-import { getWeather } from "../Helpers/api";
+import { getWeather, getWeatherByMe } from "../Helpers/api";
 import { getGeoInfo } from "../Helpers/fixData";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
 
 class App extends React.Component {
   state = {
+    input: null,
     info: {
       location: null,
       timeOffset: null
@@ -27,8 +28,33 @@ class App extends React.Component {
   };
 
   //Pings api with latitude and longitude
-  updateWeatherByCoords = (lat, lng) =>
-    getWeather(lat, lng, this.updateWeatherState);
+  updateWeatherByCoords = (data) =>
+    getWeatherByMe(data, this.updateWeatherState);
+
+  // updateWeatherByCoords = (lat, lng) =>
+  //   getWeather(lat, lng, this.updateWeatherState);
+
+  chechInput = (input) => {
+    let data = {};
+    if(isNaN(input)) {
+      console.log('string');
+      // check to see if the string includes a ',' this verifies that input must be cooridnates
+      if(input.includes(',')) {
+        console.log('this must be coordinates');
+        let array = input.split(', ');
+
+        data.lat = array[0];
+        data.lng = array[1];
+      } else {
+        data.city = input;
+      }
+      
+   } else if(!isNaN(input)) {
+     console.log('number');
+     data.zip = input;
+   }
+   return data;
+  }
 
 
   //geo-suggest on-select update
@@ -38,7 +64,11 @@ class App extends React.Component {
       this._geoSuggest.blur();
 
       let { lat, lng } = geoData.location;
-      this.updateWeatherByCoords(lat, lng);
+      let data = {
+        lat: lat,
+        lng: lng,
+      }
+      this.updateWeatherByCoords(data);
 
       let { location, timeOffset } = getGeoInfo(geoData);
       this.setState(state => ({
@@ -51,12 +81,56 @@ class App extends React.Component {
     }
   };
 
+  // onSuggestSelect = geoData => {
+  //   if (geoData) {
+  //     this._geoSuggest.clear();
+  //     this._geoSuggest.blur();
+
+  //     let { lat, lng } = geoData.location;
+  //     this.updateWeatherByCoords(lat, lng);
+
+  //     let { location, timeOffset } = getGeoInfo(geoData);
+  //     this.setState(state => ({
+  //       ...state,
+  //       info: {
+  //         location,
+  //         timeOffset
+  //       }
+  //     }));
+  //   }
+  // };
+
   //Get geolocation position, and update
   getCurrentPos = data => {
-    console.log(data);
+    console.log('getCurrentPos', data);
     let { latitude, longitude } = data.coords;
-    this.updateWeatherByCoords(latitude, longitude);
+    data = {lat: latitude, lng: longitude};
+
+    this.updateWeatherByCoords(data);
   };
+
+  // getCurrentPos = data => {
+  //   console.log('getCurrentPos', data);
+  //   let { latitude, longitude } = data.coords;
+    
+  //   this.updateWeatherByCoords(latitude, longitude);
+  // };
+
+  // Grabs the value from the input bar
+  handleGeosuggestChange = value => {
+    this.setState({
+      input: value 
+    });
+    console.log('input', isNaN(this.state.input));
+  }
+  // Send input to ...
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log('darrian', this.state.input);
+    let data = this.chechInput(this.state.input);
+    console.log('after check input', data);
+    getWeatherByMe(data, this.updateWeatherState);
+   }
 
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(this.getCurrentPos);
@@ -70,12 +144,18 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <form onSubmit={this.handleOnSubmit}>
         <Geosuggest
+          placeholder="Start typing!"
           ref={el => (this._geoSuggest = el)}
           onSuggestSelect={this.onSuggestSelect}
           autoActivateFirstSuggest={true}
           maxFixtures={5}
+          onChange={this.handleGeosuggestChange}
         />
+        <button type="submit">Search</button>
+        </form>
+
         {showWeather && <Weather info={info} data={weather} />}
         {showForecast && <Forecast info={info} data={forecast} />}
       </div>
