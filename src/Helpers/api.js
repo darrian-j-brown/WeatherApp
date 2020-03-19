c3459d21585c48b8ac3079ed9119c6fc63e48b49
Darrian Brown <darrian.jordan.brown@gmail.com>
2020-03-18 21:02:08 -0500

    added getWeatherByMe updating getWeather to handling data object instead of lat and long

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -10,3 +10,28 @@ export let getWeather = (lat, lng, update) => {
       update({ weather, forecast });
     });
 };
+
+export let getWeatherByMe = (data, update) => {
+  const { lat, lng, city, zip } = data;
+  let url = "http://localhost:8080/weather-api";
+  if(lat && lng) {
+    console.log('coor')
+    url += "?lat=" + lat;
+    url += "&lon=" + lng;
+  } else if(city) {
+    console.log('city')
+    url += "?city=" + city;
+  } else if(zip) {
+    console.log('zip')
+    url += "?zip=" + zip;
+  }
+  
+  fetch(url)
+    .then(res => res.json())
+    .then(({ weather, forecast }) => {
+      update({ weather, forecast });
+    })
+    .catch((error) => {
+      console.log('error', error)
+    })
+};
